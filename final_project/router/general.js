1ff2ef8df14a142b34141d400f5fc288fbf84951
Joey Huang <yeeeshiuan.huang@gmail.com>
2024-04-09 22:08:05 -0400

    Add isbn attribute to books db, update the get books list with array type, and add get isbn feature

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,14 +13,17 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.send(books);
+  res.send(Object.values(books));
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_books = Object.values(books).filter((book) => book.isbn === isbn);
+  res.send(filtered_books);
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
