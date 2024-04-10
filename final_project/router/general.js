85ce68b0c2803e48364d55e19425d0e52a8cf9fb
Joey Huang <yeeeshiuan.huang@gmail.com>
2024-04-09 22:11:52 -0400

    add the get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,7 +29,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_books = Object.values(books).filter((book) => book.author === author);
+  res.send(filtered_books);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
